a5bcc28a9ceb55f7195d213381fcc801dc865ddf
Arthur Lockman <hello@rthr.me>
2017-04-04 12:32:44 -0400

    Added IRangeFinderSensor

---
+++ include/IRangeFinderSensor.h
@@ -0,0 +1,7 @@
+class RangeFinderSensor : public ISensor {
+
+    public:
+        virtual void updateSensor() = 0;
+        virtual double getDistanceMM() = 0;
+        virtual double getDistanceCM() = 0;
+};
