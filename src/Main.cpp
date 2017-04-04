268d8ef876f0035c4530a9b7d356e6e4509e0356
Arthur Lockman <hello@rthr.me>
2017-04-04 12:17:29 -0400

    Added test code for threading

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,18 @@
 #include "Main.h"
 
+class MyThread : public Thread
+{
+    public:
+    void *run() {
+        for (int i = 0; i < 5; i++) {
+            printf("thread %lu running - %d\n",  (long unsigned int)self(), i+1);
+            sleep(2);
+        }
+        printf("thread done %lu\n", (long unsigned int)self());
+        return NULL;
+    }
+};
+
 int main (void)
 {
     // wiringPiSetup () ;
@@ -22,6 +35,14 @@ int main (void)
 
     comm->sendNextMsg();
     comm->sendNextMsg();
+
+    MyThread* thread1 = new MyThread();
+    MyThread* thread2 = new MyThread();
+    thread1->start();
+    thread2->start();
+    thread1->join();
+    thread2->join();
+    printf("main done\n");
 
     return 0 ;
 
