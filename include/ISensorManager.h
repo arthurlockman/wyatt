5e464a923b88ea2366f62d4fbeb7b8b7a5254aba
Arthur Lockman <hello@rthr.me>
2017-04-05 10:49:03 -0400

    Flushed out ISensorManager

--- include/ISensorManager.h
+++ include/ISensorManager.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <list>
+#include <ISensor.h>
 
+using namespace std;
 class ISensorManager{
 
 public:
-    ISensorManager();    
+    ISensorManager(list<ISensor*>* sensors) {
+        this->sensors = sensors;
+    };
 
+    virtual ~ISensorManager() = 0;
+
+    virtual void updateSensors() = 0;
+
+protected:
+    list<ISensor*>* sensors;
 };
