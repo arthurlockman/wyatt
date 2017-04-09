5eea334eb8e4033fcd3a34f665e5301e118a60ab
tuckerhaydon <tuckerhaydon@gmail.com>
2017-04-09 19:42:21 -0400

    Removed all test code from main. Now is in test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,25 +1,6 @@
-#include <commands/SimpleIteratorCommand.h>
 #include "Main.h"
 
-using namespace std;
 int main (void)
 {
-    wiringPiSetup();
 
-    /* Create sensor manager */
-    ISensorManager* sensorManager = new SensorManager();
-
-    /* Create communicator and attach serial port */
-    Communicator* comm = new Communicator(sensorManager, 9600);
-    comm->attachArduino("/dev/ttyUSB0", H_LEFT_MOTOR);
-
-    /* Create test command */
-    char commands[] = {H_LEFT_MOTOR.address, 0};
-    Message* msg1 = new Message(H_LEFT_MOTOR, commands);
-
-    /* Queue and send command */
-    comm->queueMsg(msg1);
-    comm->sendNextMsg(H_LEFT_MOTOR);
-
-    return 0 ;
 }
