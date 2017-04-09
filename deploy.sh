d278287005b793b860b76c390a4b0f123fb37410
Arthur Lockman <hello@rthr.me>
2017-04-09 13:47:23 -0400

    Moves hardware files to Arduino folder.

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,10 @@ make
 cd ..
 if [ -e /dev/ttyUSB0 ]; then
     echo "Deploying to Arduino..."
+    rm ./Arduino/Hardware.cpp
+    rm ./Arduino/Hardware.h
+    cp ./src/Hardware.cpp ./Arduino/Hardware.cpp
+    cp ./src/Hardware.h ./Arduino/Hardware.h
     /usr/local/arduino-1.8.2/arduino --board arduino:avr:nano:cpu=atmega328 --port /dev/ttyUSB0 --upload ./arduino/serialTest/serialTest.ino
 else
     echo "No Arduino connected."
