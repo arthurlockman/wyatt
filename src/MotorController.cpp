f209517b0e427d4c2a8123c324cd8901a40cce78
Arthur Lockman <hello@rthr.me>
2017-04-14 14:35:44 -0400

    Added MotorController class
    This class can be used as a common interface for talking to motor
    controllers.

---
+++ src/MotorController.cpp
@@ -0,0 +1,21 @@
+#include "MotorController.h"
+
+MotorController::MotorController()
+{
+}
+
+bool MotorController::setMotor(int motorID, int speed)
+{
+    return true;
+}
+
+bool MotorController::stopMotor(int motorID)
+{
+    return true;
+}
+
+bool MotorController::stopAllMotors()
+{
+    return true;
+}
+
