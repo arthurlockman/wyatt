0e1b21562ab6b7c5d916f642b5b830aa14a693b5
Arthur Lockman <hello@rthr.me>
2017-04-23 20:26:32 -0400

    Added doc comments to chassis.h

--- include/Chassis.h
+++ include/Chassis.h
@@ -13,24 +13,60 @@
 
 class Chassis: public IHardwareInterface {
 public:
+    /**
+     * Creates a chassis object. This also instantiates
+     * the Adafruit PWM Servo Hat module.
+     */
     Chassis();
+    /**
+     * Destructs the chassis module.
+     */
     ~Chassis() override;
+    /**
+     * Send a command to the chassis (a command
+     * to drive is expected here).
+     * @param msg The drive command message.
+     */
     void write(Message* msg) override;
+    /**
+     * Get messages from the chassis. This will never
+     * return a real value, as the chassis does not send
+     * any messages.
+     * @return NULL.
+     */
     std::list<Message*>* read() override;
 private:
+    //! Instance of the PWM Servo Hat.
     AdafruitPWMServoHat* m_pwmHat;
+    /**
+     * Enumerates the addresses on the servo hat of
+     * each of the pins for the left and right motors.
+     */
     enum m_motorAddresses: int {
         right1 = 0,
         right2,
         left1,
         left2
     };
+    /**
+     * Enumerates the message destinations within the chassis
+     * class.
+     */
     enum m_motor: int {
         LEFT_MOTOR = 0,
         RIGHT_MOTOR
     };
-
+    /**
+     * Drives a specific motor at a specific speed.
+     * @param speed A value from 0-255, motor speed.
+     */
     void driveMotor(m_motor, int speed);
+    /**
+     * Convert motor speed from 0-255 to 0-4096 to hand
+     * off to the servo hat.
+     * @param speed an int, 0-255.
+     * @return a speed, int, 0-4095
+     */
     int mapMotorSpeed(int speed);
 };
 
